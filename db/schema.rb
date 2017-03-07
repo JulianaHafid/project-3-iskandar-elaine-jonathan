09c8d3e88108188d01ff4675a7344f871b745090
Jonathan Lim <jonathanlimes@gmail.com>
2017-03-07 13:56:55 +0800

    devise form rendering still in process

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20170307051113) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                   default: "",    null: false
+    t.string   "contact",                default: "",    null: false
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
     t.boolean  "is_Admin",               default: false, null: false
