2446d9e1315ca75765fd4dc6390b65210c42ef9d
Mohammad Noh Iskandar Jamaluddin <anatraxia@gmail.com>
2017-03-12 23:39:39 +0800

    Update schema.rb for requests table with requestor_id and standin_id. Include index for requestor_id and standin_id

--- db/schema.rb
+++ db/schema.rb
@@ -26,17 +26,19 @@ ActiveRecord::Schema.define(version: 20170307075523) do
   end
 
   create_table "requests", force: :cascade do |t|
-    t.integer  "user_id"
+    t.integer  "requestor_id"
+    t.integer  "standin_id"
     t.integer  "event_id"
-    t.integer  "acceptor_id"
     t.float    "cost"
     t.text     "description"
     t.text     "status"
     t.text     "delivery_arrangement"
+    t.text     "image_url"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
     t.index ["event_id"], name: "index_requests_on_event_id", using: :btree
-    t.index ["user_id"], name: "index_requests_on_user_id", using: :btree
+    t.index ["requestor_id"], name: "index_users_on_requestor_id"
+    t.index ["standin_id"], name: "index_users_on_standin_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -61,5 +63,4 @@ ActiveRecord::Schema.define(version: 20170307075523) do
 
   add_foreign_key "events", "users"
   add_foreign_key "requests", "events"
-  add_foreign_key "requests", "users"
 end
