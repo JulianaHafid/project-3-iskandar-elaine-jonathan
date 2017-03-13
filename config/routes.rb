ac0804d841820ae7ee7756b96d69a0a9b282bd90
Mohammad Noh Iskandar Jamaluddin <anatraxia@gmail.com>
2017-03-13 10:32:55 +0800

    include controller and views for requests - index

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
 
   get 'events/index' => 'events#index'
 
+
   devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :requests
 
   get 'static/homepage' => 'static#homepage'
 
