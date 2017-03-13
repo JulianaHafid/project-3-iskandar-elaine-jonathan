74986532f270c9c25474a99f7f13c937c19f64de
elaine lim <lainex@gmail.com>
2017-03-13 10:25:08 +0800

    updated event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
+  # before_filter :authenticate_user!, only: [:new, :edit, :update, :destroy]
   mount_uploader :image_url, PictureUploader
   has_many :requests
   belongs_to :user
