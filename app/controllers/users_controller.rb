0f98dee8b3eaaee120806b7b815c729f32d89946
elaine lim <lainex@gmail.com>
2017-03-14 16:12:46 +0800

    added super admin function to change admin status of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,36 @@
 class UsersController < ApplicationController
+before_action :set_user, only: [:show, :edit, :update, :destroy]
+before_action :admin? , only: [:index, :edit,  :update]
 
+def index
+  @users= User.all
+end
+
+
+ def update
+   if @user.update(user_params)
+     redirect_to users_path
+   else
+     render 'index'
+   end
+ end
+
+private
+
+def admin?
+  if !current_user.is_Admin
+    redirect_to root_path, :alert => "Hi there, it seems that you might be lost!"
+  if !current_user
+    redirect_to :new_user_registration, :notice => "Hi there, we invite you to sign up with us"
+  end
+end
+end
+
+def set_user
+  @user = User.find(params[:id])
+end
+
+def user_params
+  params.require(:user).permit(:is_Admin)
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   resources :events
 
 
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, :controllers => { registrations: 'registrations' }, :path_prefix => 'my'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :requests
 
