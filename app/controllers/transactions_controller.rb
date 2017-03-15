1800387418653bcab131c0210dd6f2d0d5bbdf0c
Mohammad Noh Iskandar Jamaluddin <anatraxia@gmail.com>
2017-03-15 17:20:59 +0800

    connected transactions to payment gateway sandbox

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -13,26 +13,31 @@ class TransactionsController < ApplicationController
   ]
 
   def new
-    @transaction = Transaction.new
     @client_token = Braintree::ClientToken.generate
+    # @transaction = Transaction.new
+    # @transaction.request_id=params[:request_id]
+    # @transaction.amount_paid = params[:amount_paid]
   end
 
   def show
-    @transaction = Braintree::Transaction.find(params[:id])
-    @result = _create_result_hash(@transaction)
+    @braintree_transaction = Braintree::Transaction.find(params[:id])
+    @result = _create_result_hash(@braintree_transaction)
   end
 
   def create
+    @transaction = Transaction.new
+    @transaction.request_id = params[:request_id]
+    @transaction.amount_paid = params[:amount_paid]
     nonce = params[:payment_method_nonce]
     render action: :new and return unless nonce
     result = Braintree::Transaction.sale(
-      amount: "10.00",
+      amount: @transaction.amount_paid.to_s,
       payment_method_nonce: nonce
     )
 
     flash[:notice] = "Transaction successful! Enjoy!" if result.success?
     flash[:alert] = "Something is amiss. #{result.transaction.processor_response_text}" unless result.success?
-    redirect_to order_path(result.transaction.id)
+    redirect_to transaction_path(result.transaction.id)
   end
 
   def _create_result_hash(transaction)
@@ -53,9 +58,13 @@ class TransactionsController < ApplicationController
     end
   end
   private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_transaction
+    @transaction = Transaction.find(params[:id])
+  end
   # Never trust parameters from the scary internet, only allow the white list through.
   def transaction_params
-    params.require(:transaction).permit(:request_id, :amount_paid, :payment_gateway_id)
+    params.require(:transaction).permit(:request_id, :amount_paid)
   end
 
 end
