7ecaf50a9c9f9fde477b123e00183d8fd5021fb0
elaine lim <lainex@gmail.com>
2017-03-16 11:49:43 +0800

    validation for event start date done

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,16 @@ class Event < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :name, :location, :date, :time, :image_url, :user_id
+  validate :val_event_date
+
+
+  def val_event_date
+    if date <= (Date.today() + 2.days)
+      errors.add(:date, "Date of the event needs to be at least 2 days in advance")
+    end
+  end
+
+
+
+
 end
