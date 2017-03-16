ba60814eb44fad484c6d14b2f9460d2066faa81c
elaine lim <lainex@gmail.com>
2017-03-16 14:48:59 +0800

    added validation for requests updating

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,14 +4,14 @@ class Event < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :name, :location, :date, :time, :image_url, :user_id
-  validate :val_event_date
-
-
-  def val_event_date
-    if date <= (Date.today() + 2.days)
-      errors.add(:date, "Date of the event needs to be at least 2 days in advance")
-    end
-  end
+  # validate :val_event_date
+  #
+  #
+  # def val_event_date
+  #   if date <= (Date.today() + 2.days)
+  #     errors.add(:date, "Date of the event needs to be at least 2 days in advance")
+  #   end
+  # end
 
 
 
